a6298a2ec72a90f0cf920aba84cae39e2dd8d2dd
Mac <mac@kcsuraj.local>
2019-01-03 14:39:14 +0545

    add error boundry
    changes

---
+++ build/precache-manifest.8d2905cbba0e5e6ef8e869d19460070b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    revision: "229c360febb4351a89df",
+    url: "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    revision: "0e91df72b2040b29c902",
+    url: "/static/js/main.0e91df72.chunk.js"
+  },
+  {
+    revision: "b9dfd398db81b440a634",
+    url: "/static/js/1.b9dfd398.chunk.js"
+  },
+  {
+    revision: "27e8bdb4bac8e55058c3b3995a39baef",
+    url: "/index.html"
+  }
+];
