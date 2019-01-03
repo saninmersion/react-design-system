7bed07c6fd2df17917823e1040516e829c637392
Mac <mac@kcsuraj.local>
2019-01-03 13:51:07 +0545

    add sass support, added styled components for text, heading, button and directory fixes

---
+++ src/components/styled/global/base.js
@@ -0,0 +1,14 @@
+import { css } from "styled-components";
+
+const base = css`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+export default base;
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,10 +1,17 @@
 import Colors from "./colors";
+import Defaults from "./defaults";
 const { primary, sec, neutral } = Colors;
+const { fontFamily, fontSize, fontWeight, lineHeight, transition } = Defaults;
 
 const theme = {
   primary,
   sec,
-  neutral
+  neutral,
+  fontFamily,
+  fontSize,
+  fontWeight,
+  lineHeight,
+  transition
 };
 
 export default theme;
