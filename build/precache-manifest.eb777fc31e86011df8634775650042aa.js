c4ad77a7369ae710772dd3c6e367415f1f3bc2f3
Suraj <suraj.kc@yipl.com.np>
2019-01-03 14:53:45 +0545

    fix babel loader issue

--- build/precache-manifest.eb777fc31e86011df8634775650042aa.js
+++ build/precache-manifest.eb777fc31e86011df8634775650042aa.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     url: "/static/js/runtime~main.229c360f.js"
   },
   {
-    revision: "0e91df72b2040b29c902",
-    url: "/static/js/main.0e91df72.chunk.js"
+    revision: "f5a8cec5d7271cde6af0",
+    url: "/static/js/main.f5a8cec5.chunk.js"
   },
   {
     revision: "b9dfd398db81b440a634",
     url: "/static/js/1.b9dfd398.chunk.js"
   },
   {
-    revision: "27e8bdb4bac8e55058c3b3995a39baef",
+    revision: "6f30e79bec86aa6aa67defa6610d8dad",
     url: "/index.html"
   }
 ];
